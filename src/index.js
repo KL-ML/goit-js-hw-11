f0f3a0ef030e79b1e1362fd5bed0b216d0f94da4
Natalya Karpova <knataly0513@gmail.com>
2022-10-20 13:42:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
+import axios from "axios";
+import './css/styles.css';
+import { debounce } from "lodash";
+import API from './fetchImages';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+gallery.style.cssText += 'list-style-type:none;display:flex;flex-wrap:wrap;gap:40px;';
+console.log(searchForm);
+searchForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    // console.log(event.target.elements[0].value);
+    const inputValue = event.target.elements[0].value.trim();
+    console.log(inputValue);
+    if (inputValue !== '') {
+        API.fetchImages(inputValue)
+        .then(renderGallery)
+        .catch((error) => console.log(error));
+    }
+    gallery.innerHTML = "";
+}
+function renderGallery(images) {
+    gallery.innerHTML = "";
+    const markup = images.hits
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `<div class="photo-card">
+                        <img src="${webformatURL}" alt="${tags}" height=200px; loading="lazy" />
+                        <div class="info">
+                            <p class="info-item">
+                                <b>Likes</b>${likes}
+                            </p>
+                            <p class="info-item">
+                                <b>Views</b>${views}
+                            </p>
+                            <p class="info-item">
+                                <b>Comments</b>${comments}
+                            </p>
+                            <p class="info-item">
+                                <b>Downloads</b>${downloads}
+                            </p>
+                        </div>
+                    </div>`;
+        })
+        .join("");
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+
+
+
 // import debounce from 'lodash.debounce';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import './css/styles.css';
@@ -65,68 +118,3 @@
 //         countryInfo.innerHTML = "";
 //     }
 // }
-
-
-
-
-//   Создай фронтенд часть приложения поиска данных о стране по её частичному или полному имени. 
-// Посмотри демо видео работы приложения.
-//==============
-// HTTP-запросы​
-//==============
-//   Используй публичный API Rest Countries, а именно ресурс name, возвращающий массив объектов 
-// стран удовлетворивших критерий поиска. 
-// Добавь минимальное оформление элементов интерфейса.
-// Напиши функцию fetchCountries(name) которая делает HTTP-запрос на ресурс name и возвращает 
-// промис с массивом стран - результатом запроса. 
-// Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
-//==================
-// Фильтрация полей​
-//==================
-//   В ответе от бэкенда возвращаются объекты, большая часть свойств которых тебе не пригодится. 
-// Чтобы сократить объем передаваемых данных добавь строку параметров запроса - так этот 
-// бэкенд реализует фильтрацию полей. Ознакомься с документацией синтаксиса фильтров.
-//   Тебе нужны только следующие свойства:
-// - name.official - полное имя страны
-// - capital - столица
-// - population - население
-// - flags.svg - ссылка на изображение флага
-// - languages - массив языков
-//=============
-// Поле поиска​
-//=============
-//   Название страны для поиска пользователь вводит в текстовое поле input#search-box. 
-// HTTP - запросы выполняются при наборе имени страны, то есть по событию input. 
-// Но, делать запрос при каждом нажатии клавиши нельзя, так как одновременно получится много 
-// запросов и они будут выполняться в непредсказуемом порядке.
-// Необходимо применить приём Debounce на обработчике события и делать HTTP-запрос спустя 
-// 300мс после того, как пользователь перестал вводить текст. Используй пакет lodash.debounce.
-// Если пользователь полностью очищает поле поиска, то HTTP-запрос не выполняется, а разметка 
-// списка стран или информации о стране пропадает.
-// Выполни санитизацию введенной строки методом trim(), это решит проблему когда в поле ввода 
-// только пробелы или они есть в начале и в конце строки.
-//===========
-// Интерфейс​
-//===========
-//   Если в ответе бэкенд вернул больше чем 10 стран, в интерфейсе пояляется уведомление о том, 
-// что имя должно быть более специфичным. Для уведомлений используй библиотеку notiflix и 
-// выводи такую строку "Too many matches found. Please enter a more specific name.".
-//   Если бэкенд вернул от 2-х до 10-х стран, под тестовым полем отображается список найденных 
-// стран. Каждый элемент списка состоит из флага и имени страны.
-//   Если результат запроса это массив с одной страной, в интерфейсе отображается разметка 
-// карточки с данными о стране: флаг, название, столица, население и языки.
-//   ВНИМАНИЕ
-// Достаточно чтобы приложение работало для большинства стран. Некоторые страны, такие как 
-// Sudan, могут создавать проблемы, поскольку название страны является частью названия другой 
-// страны, South Sudan. Не нужно беспокоиться об этих исключениях.
-//==================
-// Обработка ошибки​
-//==================
-//   Если пользователь ввёл имя страны которой не существует, бэкенд вернёт не пустой массив, 
-// а ошибку со статус кодом 404 - не найдено. Если это не обработать, то пользователь никогда 
-// не узнает о том, что поиск не дал результатов.
-//   Добавь уведомление "Oops, there is no country with that name" в случае ошибки используя 
-// библиотеку notiflix.
-//   ВНИМАНИЕ
-// Не забывай о том, что fetch не считает 404 ошибкой, поэтому необходимо явно отклонить 
-// промис чтобы можно было словить и обработать ошибку.
