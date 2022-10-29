99745827e8c4e18c6982c7d664abd714d2d393c2
Natalya Karpova <knataly0513@gmail.com>
2022-10-29 23:11:45 +0300

    Update images-seach.js

--- src/images-seach.js
+++ src/images-seach.js
@@ -1,18 +1,20 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from "axios";
+import LoadMoreBtn from "./load-more-btn";
 
 const API_KEY = `30636701-b7bfaf1719dc5d89c8acde7b5`;
 const BASE_URL = `https://pixabay.com/api/`;
-        
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '.load-more',
+    hidden: true,
+});    
 
 export default class ImagesApiService {
     constructor() {
         this.searchImgQuery = '';
         this.pageNumber = 1;
     }
-
     async fetchImages() {
-        console.log('до пошуку номер сторінки:', this.pageNumber);
         const searchParams = new URLSearchParams({
             key: API_KEY,
             q: this.searchImgQuery,
@@ -26,16 +28,16 @@ export default class ImagesApiService {
         try {
             const response = await axios.get(url);
             const images = response.data;
-            if (images.total === 0) {
-                Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            }
+            // if (images.total === 0) {
+            //     Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            //     loadMoreBtn.hide();
+            // }
             this.incrementPage();
-            console.log('після пошуку номер сторінки:', this.pageNumber);
             return images;
 
         } catch (errors) {
-            console.error(errors);
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            Notify.failure("Sorry, something goes wrong. Please, check your internet-connection, or try again later.");
+            loadMoreBtn.hide();
         }     
     }
 
